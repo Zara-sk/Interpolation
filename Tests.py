4112dcb3a4a49e1c7c28dfbf1c827793a4d32f47
Zara <zara.forgest@gmail.com>
2021-03-27 22:15:27 +0700

    Update Tests.py

--- Tests.py
+++ Tests.py
@@ -66,17 +66,17 @@ if __name__ == '__main__':
 	x2_1 = lambda x, a, b: 2*x*a**2+2*a*b
 	x2_3 = lambda x, a, b: 2*a**2
 
-	# 	test_derivate_p0(x2_0, 0.1, 1, 0.14, "x^2")
+	test_derivate_p0(x2_0, 0.1, 1, 0.14, "x^2")
 	# for h=0.1:   0.04183178196587348
 	# for h=0.05:  0.038000000000209866
 	# for h=0.025: 0.014636729734741562
 	
-	#	 test_derivate_p1(x2_0, x2_1, 0.1, 0.78, 0.49, "x^2")
+	test_derivate_p1(x2_0, x2_1, 0.1, 0.78, 0.49, "x^2")
 	# for h=0.1:   2.4043999999997503
 	# for h=0.05:  2.3543999999979217
 	# for h=0.025: 2.329399999987027
 
-	# 	test_derivate_p2(x2_0, x2_3, 0.1, 0.88, 0.14, "x^2")
+	test_derivate_p2(x2_0, x2_3, 0.1, 0.88, 0.14, "x^2")
 	# for h=0.1:   0.7153016151377547
 	# for h=0.05:  0.4532619104571509
 	# for h=0.025: 0.4518608589528599
@@ -87,41 +87,38 @@ if __name__ == '__main__':
 	x3_1 = lambda x, a, b: 3*a*(a*x + b)**2
 	x3_2 = lambda x, a, b: 6*a**2*(a*x + b)
 
-	# 	test_derivate_p0(x3_0, 0.1, 1, 0.2, "x^3")
+	test_derivate_p0(x3_0, 0.1, 1, 0.2, "x^3")
 	# for h=0.1:   2.466041006996761
 	# for h=0.05:  1.3062500005738684
 	# for h=0.025: 0.6715312499968604
 
-	test_derivate_p1(x3_0, x3_1, 0.1, 0.74, 0.22, "x^3")
+	test_derivate_p1(x3_0, x3_1, 0.005, 0.74, 0.13, "x^3")
+	# for h=0.005:   15.702997999019644
+	# for h=0.0025:  15.647110507876079
+	# for h=0.00125: 15.619157370000877
 
-	test_derivate_p2(x3_0, x3_2, 0.1, 0.97, 0.33, "x^3")
+	test_derivate_p2(x3_0, x3_2, 0.1, 0.96, 0.38, "x^3")
+	# for h=0.1:   4.243030664220711
+	# for h=0.05:  4.243007999347881
+	# for h=0.025: 4.243007999845489
 
-	table = {i: x3_0(i) for i in arange(0, 10, 0.05)}
-	y1, y2, y3 = Splining(x3_0, 0.1, 0.74, 0.22, 2)
-	fig = plt.figure(figsize=[8,6])
-	plt.plot([(0.74*x + 0.22) for x in range(10)], [x3_2(x, 0.74, 0.22) for x in range(10)], label="Точная")
-	plt.plot([(0.74*x + 0.22) for x in range(10)], [y1[x] for x in range(10)], label="Апроксимирующая h = 0.1")
-	plt.plot([(0.74*x + 0.22) for x in range(10)], [y2[x] for x in range(10)], label="Апроксимирующая h = 0.05")
-	plt.plot([(0.74*x + 0.22) for x in range(10)], [y3[x] for x in range(10)], label="Апроксимирующая h= 0.0025")
-	plt.legend()
-	plt.show()
 
 	"""Test #3: f(x) = x^4"""
 	x4_0 = lambda x: x**4
 	x4_1 = lambda x, a, b: 4*a*(a*x + b)**3
 	x4_2 = lambda x, a, b: 12*a**2*(a*x + b)**2
 
-	# 	test_derivate_p0(x4_0, 0.1, 1, 0.28, "x^4")
-	# for h=0.1:   36.827566836562255
-	# for h=0.05:  14.68398421148413
-	# for h=0.025: 2.5207741506319508
+	test_derivate_p0(x4_0, 0.005, 1, 0.28, "x^4")
+	# for h=0.005:   2.5687878925500627
+	# for h=0.0025:  1.2880837669526954
+	# for h=0.00125: 0.6449647885410741
 
-	# test_derivate_p1(x4_0, x4_1, 0.1, 0.1, 0.91, "x^4")
+	test_derivate_p1(x4_0, x4_1, 0.005, 0.1, 0.91, "x^4")
 	# for h=0.1:   3.193923599999476
 	# for h=0.05:  2.534668600000097
 	# for h=0.025: 2.2628892749994987
 
-	# test_derivate_p2(x4_0, x4_2, 0.1, 1, 0.22, "x^4")
+	test_derivate_p2(x4_0, x4_2, 0.1, 1, 0.22, "x^4")
 	# for h=0.1:   0.005450198395806183
 	# for h=0.05:  0.0022127018922201147
 	# for h=0.025: 5.000468240723421e-05
@@ -132,10 +129,30 @@ if __name__ == '__main__':
 	sinx_1 = lambda x, a, b: a*cos(a*x+b)
 	sinx_2 = lambda x, a, b: -a**2*sin(a*x+b)
 
-	# test_derivate_p0(sinx_0, 0.1, 0.9, 0.1, "sin(x)")
+	test_derivate_p0(sinx_0, 0.1, 0.9, 0.1, "sin(x)")
 	# for h=0.1:   0.04426814024894654
 	# for h=0.05:  0.02349664272701435
 	# for h=0.025: 0.012085937041755979
 
-	# test_derivate_p1(sinx_0, sinx_1, 0.1, 0.73, 0.22, "sinx")
-	# test_derivate_p2(sinx_0, sinx_2, 0.1, 1, 0.21, "sinx")
+	test_derivate_p1(sinx_0, sinx_1, 0.005, 1, 0.41, "sinx")
+	# for h=0.005:   0.49081372522355826
+	# for h=0.0025:  0.4921823194986039
+	# for h=0.00125: 0.4928662936090835
+
+	test_derivate_p2(sinx_0, sinx_2, 0.01, 0.9, 0.23, "sinx")
+	# for h=0.1:   0.00020914585513298611
+	# for h=0.05:  8.60865765999197e-05
+	# for h=0.025: 2.029435198114271e-06
+
+	if 0:
+		table = {i: sinx_0(i) for i in arange(0, 10, 0.005)}
+		y1, y2, y3 = Splining(sinx_0, 0.005, 0.74, 0.22, 0)
+		fig = plt.figure(figsize=[8,6])
+		# plt.plot([(0.74*x + 0.22) for x in range(10)], [sinx_0(x, 0.74, 0.22) for x in range(10)], label="Точная")
+		plt.plot([(0.74*x + 0.22) for x in range(10)], [sinx_0(0.74*x + 0.22) for x in range(10)], label="Точная")
+		plt.plot([(0.74*x + 0.22) for x in range(10)], [y1[x] for x in range(10)], label="Апроксимирующая h = 0.1")
+		plt.plot([(0.74*x + 0.22) for x in range(10)], [y2[x] for x in range(10)], label="Апроксимирующая h = 0.05")
+		plt.plot([(0.74*x + 0.22) for x in range(10)], [y3[x] for x in range(10)], label="Апроксимирующая h= 0.0025")
+		plt.legend()
+		# plt.title("Первая производная функции x^4")
+		plt.show()
